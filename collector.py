05cd1eb4c2a293586d758bea24be00f94a334f5c
Aaron M. Shea <ashea@cs.hartford.edu>
2016-10-19 14:04:27 -0400

    Update collector.py

--- collector.py
+++ collector.py
@@ -3,23 +3,35 @@ import threading
 
 # Read keywords into a list stripping newlines
 keywords = [keyword.rstrip('\n') for keyword in open('keywords.txt')]
-print keywords
+fileOpen = open("output.txt", "a")
+for k in keywords:
+	fileOpen.write("@ATTRIBUTE\t" + k +  "\tNUMERIC\n")
+fileOpen.write("\n\n")
+fileOpen.close()
+
 
 # Read URLs into a list stripping newlines
-urls = [url.rstrip('\n') for url in open('urls.txt')]
-print urls
+urls = [url.rstrip('\n') for url in open('links.txt')]
+
 
 def processKeywords(inUrl):
-    matrix = []
-    r = requests.get(inUrl)
-    for word in keywords:
-        if word in r.text:
-            matrix.insert(0, (word, 1))
-        else:
-            matrix.insert(0, (word, 0))
-    print matrix
+	print inUrl
+	matrix = []
+	outputMatrix = []
+	r = requests.get(inUrl)
+	for word in keywords:
+		if word in r.text:
+			matrix.insert(0, (word, 1))
+			outputMatrix.insert(0, '1');
+		else:
+			matrix.insert(0, (word, 0))
+			outputMatrix.insert(0, '0');
+	# print matrix
+	fileOpen = open("output.txt", "a")
+	outstr = ','.join(outputMatrix)
+	fileOpen.write(outstr + "," + inUrl + "\n")
+	fileOpen.close()
     
 
 for url in urls:
-    processKeywords(url)
-    t = threading.Thread(target=processKeywords, args=(url,))
+	processKeywords(url)
